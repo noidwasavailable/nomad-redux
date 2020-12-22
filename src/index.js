0972505da8b0aef07cda2f3a29363bdafaaf886c
noidwasavailable <choi9601@gmail.com>
2020-12-22 18:29:46 +0900

    added dispatch and actions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,22 @@ const addButton = document.getElementById("add");
 const subtractButton = document.getElementById("subtract");
 const output = document.getElementById("countOutput");
 
-const countModifier = (count = 0) => {
+// count is going to be undefined, so we set it as 0 as default.
+const countModifier = (count = 0, action) => {
+	if (action.type === "add") count++;
+	else if (action.type === "subtract") count--;
+	console.log(count);
 	return count;
 };
 
 const countStore = createStore(countModifier);
+
+const addHandler = () => {
+	countStore.dispatch({ type: "add" });
+};
+const subtractHandler = () => {
+	countStore.dispatch({ type: "subtract" });
+};
+
+addButton.addEventListener("click", addHandler);
+subtractButton.addEventListener("click", subtractHandler);
