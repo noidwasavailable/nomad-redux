38759f7e9a182acf78db067eb095689d5833d923
noidwasavailable <choi9601@gmail.com>
2020-12-22 19:00:52 +0900

    changed to switch case

--- src/index.js
+++ src/index.js
@@ -4,20 +4,28 @@ const addButton = document.getElementById("add");
 const subtractButton = document.getElementById("subtract");
 const output = document.getElementById("countOutput");
 
+const ADD = "add";
+const SUBTRACT = "subtract";
+
 // count is going to be undefined, so we set it as 0 as default.
 const countModifier = (count = 0, action) => {
-	if (action.type === "add") count++;
-	else if (action.type === "subtract") count--;
-	return count;
+	switch (action.type) {
+		case ADD:
+			return count + 1;
+		case SUBTRACT:
+			return count - 1;
+		default:
+			return count;
+	}
 };
 
 const countStore = createStore(countModifier);
 
 const addHandler = () => {
-	countStore.dispatch({ type: "add" });
+	countStore.dispatch({ type: ADD });
 };
 const subtractHandler = () => {
-	countStore.dispatch({ type: "subtract" });
+	countStore.dispatch({ type: SUBTRACT });
 };
 
 const broadcastCount = () => {
